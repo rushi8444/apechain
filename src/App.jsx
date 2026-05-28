8ab887b44b6ce36ef53bffa4abb6a6dfdfbdb704
Aniket-890 <aniket.gunavare23@pcu.edu.in>
2026-05-28 19:49:01 +0530

    Spotlight changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,60 @@
 import './App.css'
 import Home from './pages/Home'
+import Spotlight from './pages/Spotlight'
 import Loader from './components/HomePage/Loader'
+import Layout from './components/common/Layout'
+
 import { useEffect, useState } from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
 
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData =  () => {
+    const fetchData = () => {
       setTimeout(() => {
         setLoading(false);
-      },4000);
+      }, 4000);
     }
-      fetchData();
-  },[])
-  
+
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      {/* {
-         loading ? <Loader  /> : <Home/>
-      } */}
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div>
+        {
+          loading ? (
+            <Loader />
+          ) : (
+            <Routes>
+
+              {/* Home page unchanged */}
+              <Route
+                path="/"
+                element={<Home />}
+              />
+
+              {/* Spotlight with Navbar + Footer */}
+              <Route
+                path="/spotlight"
+                element={
+                  <Layout>
+                    <Spotlight />
+                  </Layout>
+                }
+              />
+
+            </Routes>
+          )
+        }
+      </div>
+    </BrowserRouter>
   )
 }
 
