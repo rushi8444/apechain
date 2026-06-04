7069cce8770f27c7fb4bb9e2a309d495b2b16d94
rushi8444 <rushikeshbagal8444@gmail.com>
2026-06-04 20:37:25 +0530

    Fixed Import in Spotlight

--- src/pages/Spotlight.jsx
+++ src/pages/Spotlight.jsx
@@ -1,3 +1,12 @@
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
+import SpotHero from "../components/Spotlight/SpotHero";
+import RiseCards from "../components/Spotlight/RiseCards";
+import RoundTwo from "../components/Spotlight/RoundTwo";
+import ReadMore from "../components/Spotlight/ReadMore";
+import Reviews from "../components/Spotlight/Reviews";
+import SeasonCard from "../components/Spotlight/SeasonCard";
+
 function Spotlight() {
   return (
     <main className="bg-[#050816] text-white min-h-screen overflow-x-hidden">
@@ -17,4 +26,4 @@ function Spotlight() {
   );
 }
 
-export default Apps;
+export default Spotlight;
